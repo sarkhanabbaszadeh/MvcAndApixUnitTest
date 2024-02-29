9c59b10a7ff9dc604242215da40843f16ab63adb
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-02-29 11:28:01 +0400

    add repository

---
+++ MvcAndApixUnitTest.Web/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MvcAndApixUnitTest.Web.Models;
+
+namespace MvcAndApixUnitTest.Web.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly XUnitTestDbContext _context;
+        private readonly DbSet<TEntity> _entities;
+
+        public Repository(XUnitTestDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<TEntity>();
+        }
+
+        public async Task Create(TEntity entity)
+        {
+            await _entities.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _entities.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _entities.FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _entities.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
