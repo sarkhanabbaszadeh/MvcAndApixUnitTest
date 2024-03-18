2e3b427f364bb54bbc4a0f36e4abd8d1a9d5e165
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-03-18 14:41:00 +0400

    Testing the Delete Confirmed Method delete method execute scenario

--- MvcAndApixUnitTest.Test/ProductControllerTest.cs
+++ MvcAndApixUnitTest.Test/ProductControllerTest.cs
@@ -282,5 +282,18 @@ namespace MvcAndApixUnitTest.Test
             Assert.IsType<RedirectToActionResult>(result);
 
         }
+
+        [Theory,InlineData(1)]
+        public async void DeleteConfirmed_ActionExecutes_DeleteMethodExecute(int productId)
+        {
+            var product = _products.First(x => x.Id == productId);
+
+            _repositoryMock.Setup(repo => repo.Delete(product));
+
+            await _productsController.DeleteConfirmed(productId);
+
+            _repositoryMock.Verify(repo => repo.Delete(It.IsAny<Product>()), Times.Once());
+
+        }
     }
 }
