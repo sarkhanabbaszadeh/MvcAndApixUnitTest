b0a728dd6c752e411050a2bde89730d5bdd0793a
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-03-19 11:09:14 +0400

    Testing the GetProduct Api id in valid scenario

--- MvcAndApixUnitTest.Test/ProductApiControllerTest.cs
+++ MvcAndApixUnitTest.Test/ProductApiControllerTest.cs
@@ -43,5 +43,18 @@ namespace MvcAndApixUnitTest.Test
             Assert.Equal<int>(2,returnProducts.ToList().Count);
 
         }
+
+        [Theory,InlineData(0)]
+        public async void GetProduct_IdInValid_ReturnNotFound(int productId)
+        {
+            Product product = null;
+
+            _mockRepo.Setup(x=>x.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.GetProduct(productId);
+
+            Assert.IsType<NotFoundResult>(result);
+
+        }
     }
 }
